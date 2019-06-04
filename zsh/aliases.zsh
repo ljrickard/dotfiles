ff25c54e477cc2c4ec79651728dc98e6b01d0b05
Luke Rickard <luke.rickard@sojern.com>
2019-06-04 15:59:56 +0100

    add kubectl related stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -20,3 +20,19 @@ alias gup="git add  . ; git commit --amend --no-edit && git push origin HEAD:ref
 
 # gloud
 alias gcp="gcloud"
+
+alias k='kubectl'
+# https://learnk8s.io/blog/kubectl-productivity/
+# Get current context
+alias krc='kubectl config current-context'
+# List all contexts
+alias klc='kubectl config get-contexts -o name | sed "s/^/  /;\|^  $(krc)$|s/ /*/"'
+# Change current context
+alias kcc='kubectl config use-context "$(klc | fzf -e | sed "s/^..//")"'
+
+# Get current namespace
+alias krn='kubectl config get-contexts --no-headers "$(krc)" | awk "{print \$5}" | sed "s/^$/default/"'
+# List all namespaces
+alias kln='kubectl get -o name ns | sed "s|^.*/|  |;\|^  $(krn)$|s/ /*/"'
+# Change current namespace
+alias kcn='kubectl config set-context --current --namespace "$(kln | fzf -e | sed "s/^..//")"'
